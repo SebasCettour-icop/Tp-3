0fdb35f8178cab0ab33739b8aaf370e565f13ff1
Sebastián Cettour <sebas.cettour83@gmail.com>
2025-06-09 23:46:38 -0300

    Mejoras

--- src/resol/CettourS/ejercicio2/Ejercicio2.java
+++ src/resol/CettourS/ejercicio2/Ejercicio2.java
@@ -4,6 +4,8 @@ public class Ejercicio2 {
     public static void ejecutar() {
         Vehiculo v1 = new Vehiculo("Toyota Hilux", 2020, 140000);
         Vehiculo v2 = new Vehiculo("Ford Ranger", 2018, 187000);
+        Vehiculo v3 = new Vehiculo("Chevrolet S10", 2021, 90000);
+        Vehiculo v4 = new Vehiculo("Volkswagen Amarok", 2019, 120000);
 
         System.out.println("----- Vehículo 1 -----");
         v1.mostrarInformacion();
@@ -14,5 +16,15 @@ public class Ejercicio2 {
         v2.mostrarInformacion();
         v2.recorrer(1300);
         v2.proximoServicio();
+
+        System.out.println("\n----- Vehículo 3 -----");
+        v3.mostrarInformacion();
+        v3.recorrer(850);
+        v3.proximoServicio();
+
+        System.out.println("\n----- Vehículo 4 -----");
+        v4.mostrarInformacion();
+        v4.recorrer(400);
+        v4.proximoServicio();
     }
 }
